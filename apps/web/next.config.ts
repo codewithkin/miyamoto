53f41c1453c41bd56fe056658eb03dd3a14227f2
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-09-06 19:20:17 +0200

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,11 @@
+import "@miyamoto/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+  output: "standalone",
+  transpilePackages: ["shiki"],
+};
+
+export default nextConfig;
